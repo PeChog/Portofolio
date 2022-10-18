1f4fc7f860b3a5b38da57092a9c8db2aeb0ef2de
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2022-10-18 13:55:09 +0200

    create app architecture init fonts router create

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="social">
+        <span></span>
+        <span>IG</span>
+        <span>IN</span>
+        <span>Github</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
